2af0bb0c8b7a11cbd3861dd3e0199cb152f27d4b
snikhil1998 <snsreddy1988@gmail.com>
2020-05-09 10:15:44 +0530

    Made Calculator user-driven

--- src/main/java/com/calculator/Calculator.java
+++ src/main/java/com/calculator/Calculator.java
@@ -1,33 +1,82 @@
 package com.calculator;
 
-public class Calculator {
+import java.util.Scanner;
 
-	public static void main(String[] args){
-		System.out.println("6 + 3 = " + add(6, 3));
-		System.out.println("6 - 3 = " + subtract(6,3));
-		System.out.println("6 * 3 = " + multiply(6,3));
-		System.out.println("6 / 3 = " + divide(6,3));
-	}
-
-	public static int add(int a, int b) {
+public class Calculator
+{
+	public static int add(int a, int b)
+	{
 		return a + b;
 	}
-	
-	public static int subtract(int a, int b) {
+	public static int subtract(int a, int b)
+	{
 		return a - b;
 	}
-
-	public static long multiply(int a, int b) {
+	public static long multiply(int a, int b)
+	{
 		return a * b;
 	}
-
-	public static double divide(int a, int b) {
+	public static double divide(int a, int b)
+	{
 		double result;
-		if (b == 0) {
-			throw new IllegalArgumentException("Divisor cannot divide by zero");
-		} else {
+		if (b == 0)
+		{
+			throw new IllegalArgumentException("Cannot divide by zero");
+		}
+		else
+		{
 			result = Double.valueOf(a)/Double.valueOf(b);
 		}
 		return result;
 	}
+
+	public static void main(String[] args){
+		String[][] oper={{"Addition","+"},{"Subtraction","-"},{"Multiplication","*"},{"Division","/"}};
+		Scanner reader=new Scanner(System.in);
+		boolean flag=true;
+		while(flag)
+		{
+			System.out.println("1) Addition");
+			System.out.println("2) Subtraction");
+			System.out.println("3) Multiplication");
+			System.out.println("4) Division");
+			System.out.println("5) Exit\n");
+			System.out.print("Enter choice: ");
+			int ch=reader.nextInt();
+			if(ch<1 || ch>5)
+			{
+				System.out.println();
+			}
+			else if(ch==5)
+			{
+				flag=false;
+			}
+			else
+			{
+				System.out.println(oper[ch-1][0]);
+				System.out.print("a = ");
+				int a = reader.nextInt();
+				System.out.print("b = ");
+				int b = reader.nextInt();
+				System.out.print("a"+oper[ch-1][1]+"b = ");
+				if(ch==1)
+				{
+					System.out.println(add(a,b)+"\n");
+				}
+				else if(ch==2)
+				{
+					System.out.println(subtract(a,b)+"\n");
+				}
+				else if(ch==3)
+				{
+					System.out.println(multiply(a,b)+"\n");
+				}
+				else
+				{
+					System.out.println(divide(a,b)+"\n");
+				}
+			}
+		}
+	}
+
 }
